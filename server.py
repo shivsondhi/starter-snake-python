b5863a79776c585ce9eb42bf91e3e83c40c05898
Shiv Sondhi <shiv.codes.22@gmail.com>
2020-04-10 19:51:42 -0400

    changed color and appearance

--- server.py
+++ server.py
@@ -1,6 +1,5 @@
 import os
 import random
-
 import cherrypy
 
 """
@@ -18,7 +17,7 @@ class Battlesnake(object):
     @cherrypy.expose
     def ping(self):
         # The Battlesnake engine calls this function to make sure your snake is working.
-        return "pong"
+        return "Howdy!"
 
     @cherrypy.expose
     @cherrypy.tools.json_in()
@@ -28,8 +27,9 @@ class Battlesnake(object):
         # cherrypy.request.json contains information about the game that's about to be played.
         # TODO: Use this function to decide how your snake is going to look on the board.
         data = cherrypy.request.json
+        print(data)
         print("START")
-        return {"color": "#888888", "headType": "regular", "tailType": "regular"}
+        return {"color": "#000000", "headType": "fang", "tailType": "round-bum"}
 
     @cherrypy.expose
     @cherrypy.tools.json_in()
@@ -39,12 +39,10 @@ class Battlesnake(object):
         # Valid moves are "up", "down", "left", or "right".
         # TODO: Use the information in cherrypy.request.json to decide your next move.
         data = cherrypy.request.json
-
         # Choose a random direction to move in
         possible_moves = ["up", "down", "left", "right"]
         move = random.choice(possible_moves)
-
-        print(f"MOVE: {move}")
+        print("MOVE: {}".format(move))
         return {"move": move}
 
     @cherrypy.expose
